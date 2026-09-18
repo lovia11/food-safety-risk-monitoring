c5ee666d738a4c4c1d5b2f797c1cb3e095936bba
lovia11 <songyilian7@gmail.com>
2026-09-18 23:44:05 +0800

    Update runtime method inventory for b9

--- tests/test_inspection_runtime.py
+++ tests/test_inspection_runtime.py
@@ -110,7 +110,7 @@ class InspectionRuntimeTest(unittest.TestCase):
         )
 
         self.assertEqual(first, second)
-        self.assertEqual(store.table_counts()["inspection_methods"], 7)
+        self.assertEqual(store.table_counts()["inspection_methods"], 9)
         self.assertEqual(store.table_counts()["risk_substance_mappings"], 81)
         with sqlite3.connect(store.database_path) as connection:
             self.assertEqual(connection.execute("PRAGMA user_version").fetchone()[0], 14)
